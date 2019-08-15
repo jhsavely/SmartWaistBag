3ad082ca67183ebf1053473ad7ed5f067cd1d11d
John Savely <jh.savely@gmail.com>
2019-08-15 18:14:17 +0300

    separated classes

--- lib/main.dart
+++ lib/main.dart
@@ -7,33 +7,39 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final title = 'smartwaistbag';
-
     return MaterialApp(
-      title: title,
+      debugShowCheckedModeBanner: false,
       theme: new ThemeData(
           primaryTextTheme: TextTheme(title: TextStyle(color: Colors.blue))),
-      home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.white,
-          title: Text(title),
-          centerTitle: true,
-        ),
-        body: GridView.count(
-          // Create a grid with 2 columns. If you change the scrollDirection to
-          // horizontal, this produces 2 rows.
-          crossAxisCount: 3,
-          // Generates  widgets that display their index in the List.
-          children: List.generate(15, (index) {
-            return Center(
-              child: Text(
-                'Item $index',
-                style: Theme.of(context).textTheme.headline,
-              ),
-            );
-          }),
-        ),
+      home: new MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return new Scaffold(
+
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: Text('smartwaistbag'),
+        centerTitle: true,
+      ),
+      body: new GridView.count(
+        crossAxisCount: 3,
+        children: new List<Widget>.generate(6, (index) {
+          return new GridTile(
+            child: new Card(
+                color: Colors.blue.shade200,
+                child: new Center(
+                  child: new Text('tile $index'),
+                )
+            ),
+          );
+        }),
       ),
     );
   }
 }
+
