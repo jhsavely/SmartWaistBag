10d56edb56a90059a039380e02e79c2ba20cda79
John Savely <jh.savely@gmail.com>
2019-08-17 21:49:01 +0300

    added permission and key

--- lib/main.dart
+++ lib/main.dart
@@ -10,36 +10,53 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: new ThemeData(
-          primaryTextTheme: TextTheme(title: TextStyle(color: Colors.blue))),
-      home: new MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget{
-  @override
-  Widget build(BuildContext context){
-    return new Scaffold(
-
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        title: Text('smartwaistbag'),
-        centerTitle: true,
-      ),
-      body: new GridView.count(
-        crossAxisCount: 3,
-        children: new List<Widget>.generate(6, (index) {
-          return new GridTile(
-            child: new Card(
-                color: Colors.blue.shade200,
-                child: new Center(
-                  child: new Text('tile $index'),
-                )
+          primaryTextTheme: TextTheme(title: TextStyle(color: Colors.white))),
+      home: DefaultTabController(
+        length: 1,
+        child: Scaffold(
+            appBar: AppBar(
+              backgroundColor: Colors.lightBlue,
+              title: Text('smartwaistbag'),
+              centerTitle: true,
+              bottom: TabBar(
+                tabs: [
+                  Tab(
+                    text: 'Charge 76%',
+                  ),
+                ],
+                isScrollable: true,
+              ),
             ),
-          );
-        }),
+            body: TabBarView(children: [
+              GridView(
+                scrollDirection: Axis.vertical,
+                controller: ScrollController(),
+                gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+                    maxCrossAxisExtent: 200.0),
+                children: List.generate(6, (index) {
+                  return Container(
+                    padding: EdgeInsets.all(20.0),
+                    child: Center(
+                      child: GridTile(
+                        footer: Text(
+                          'Item $index',
+                          textAlign: TextAlign.center,
+                        ),
+                        header: Text(
+                          'SubItem $index',
+                          textAlign: TextAlign.center,
+                        ),
+                        child: Icon(Icons.settings,
+                            size: 40.0, color: Colors.white30),
+                      ),
+                    ),
+                    color: Colors.blue[400],
+                    margin: EdgeInsets.all(1.0),
+                  );
+                }),
+              ),
+            ])),
       ),
     );
   }
 }
-
